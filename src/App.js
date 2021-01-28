661895070f0cd2ded2123d1489ef8cbddd0e6c80
Mai Yia Xiong <maiyiax@gmail.com>
2021-01-27 18:26:33 -0600

    updataed photos"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
@@ -24,10 +24,8 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
+          <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
-        </div>
       </main>
     </div>
   );
